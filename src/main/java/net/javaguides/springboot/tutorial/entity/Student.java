b0b2c973e3fbc603f0ecea7539065da7bc4ca96e
vikasgupta89 <gupta.vikas985@gmail.com>
2023-06-25 22:58:49 +0530

    My First Commit

---
+++ src/main/java/net/javaguides/springboot/tutorial/entity/Student.java
@@ -0,0 +1,70 @@
+package net.javaguides.springboot.tutorial.entity;
+
+
+import jakarta.persistence.*;
+//import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name="student")
+public class Student { 
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    
+    //@NotBlank(message = "Name is mandatory")
+    @Column(name = "name")
+    private String name;
+    
+    //@NotBlank(message = "Email is mandatory")
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone_no")
+    private long phoneNo;
+    
+    public Student() {}
+
+    public Student(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+    public long getId() {
+        return id;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+	public long getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(long phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", email=" + email + ", phoneNo=" + phoneNo + "]";
+	}
+	
+}
